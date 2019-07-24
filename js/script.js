5ea0f79744fbcc6205a7874a40a98e42006071ad
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2019-07-24 08:46:22 +0200

    New files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const icon = document.querySelector('.hamburger');
+// const menu = document.querySelector('.nav-right');
+icon.addEventListener("click", function () {
+    icon.classList.toggle("active");
+    // menu.classList.toggle("active");
+});
