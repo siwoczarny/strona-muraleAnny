c66e81e44cdd291a5ea73e61fbea599d568367e8
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2019-07-26 13:14:03 +0200

    Poprawki

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+// Header po scrollu
+
+window.onload = function () {
+
+    window.onscroll = function () {
+
+        var header = document.getElementById("header");
+        var yScrollAxis = window.pageYOffset;
+
+        if (yScrollAxis > 100)
+            header.style.backgroundColor = "#303030";
+        else
+            header.style.backgroundColor = "transparent";
+
+
+    };
+};
+
+// Hamburger i menu
+
 const icon = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-menu');
 icon.addEventListener("click", function () {
